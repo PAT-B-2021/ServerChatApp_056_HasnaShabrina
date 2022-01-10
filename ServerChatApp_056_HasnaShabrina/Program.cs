82381bcfe5d9797abdf53a5afbf6b1ec5cb81e40
hasnashab <78135389+hasnashab@users.noreply.github.com>
2022-01-11 00:03:53 +0700

    update

---
+++ ServerChatApp_056_HasnaShabrina/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfService_2Way_056;
+
+namespace ServerChatApp_056_HasnaShabrina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
